9b5deece0db7e2b4163ee91ecc0e450debfc9757
unknown <eth13@mg.ad.cs.pitt.edu>
2011-11-03 17:56:36 -0400

    "Added functionality to output random answers produced to a file"

--- src/cs2731/DeanQA.java
+++ src/cs2731/DeanQA.java
@@ -4,7 +4,9 @@ package cs2731;
 
 import java.util.List;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 import static java.lang.System.*;
@@ -17,6 +19,7 @@ public class DeanQA
 
 	static List<String> lines;
 	static List<String> questions;
+	static RandomAnswerFinder randomAnswerer;
 	
 	static void printUsage() {
 		out.println("Usage: DeanQA input_filename outputfile_name");
@@ -25,6 +28,8 @@ public class DeanQA
 	
 	
 	public static void main(String[] args) throws IOException {
+		int answerLine, questionNumber = 1;
+		String questionWithTag;
 		
 		// get the input and output file paths from the command line:
 		if (args.length < 1) {
@@ -37,7 +42,7 @@ public class DeanQA
 			printUsage();
 		}
 
-		// read file into an arraylist
+		// read file into an list
 		Scanner input = new Scanner(new File(args[0]));
 		lines = new ArrayList<String>();
 		questions = new ArrayList<String>();
@@ -49,15 +54,30 @@ public class DeanQA
 			lines.add(line);
 		}
 		
+		//Strip off the <Q?> tag from the question
 		while (input.hasNextLine()) {
-			questions.add(input.nextLine());
+			questionWithTag = input.nextLine();
+			questions.add(questionWithTag.substring(questionWithTag.lastIndexOf(">") + 2));
 		}
 		
 		input.close();
 
-		out.println(System.getProperty("user.dir"));
-		out.println(lines);
-		out.println(questions);
+		// finds the answers to the questions by getting them from randomAnswerer
+		// answer lines start counting from 1! 
+		randomAnswerer = new RandomAnswerFinder();
+		PrintWriter output = new PrintWriter(new FileWriter(args[1]));
+		output.println("<FILE>" + args[1].substring(args[1].lastIndexOf("/") + 1));
+		
+		for(String question : questions) {
+			answerLine = randomAnswerer.getAnswerLine(lines, question);
+			output.println("<Q_NUMBER>" + questionNumber);
+			output.println("<A_LINE>" + (answerLine + 1));
+			output.println("<Q_TXT>" + question);
+			output.println("<A_TXT>" + lines.get(answerLine));
+			output.println("");
+			questionNumber++;
+		}
+		output.close();
 		
 	}
 }
