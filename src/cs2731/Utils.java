d73b3ea4b3220f10d3d661ceb4c1889615bdd3c5
Eric Heim <eheim602@gmail.com>
2011-11-12 16:55:18 -0500

    Added the "extractAnswers" method in Utils to etract the answers from an answer file for training.

--- src/cs2731/Utils.java
+++ src/cs2731/Utils.java
@@ -1,6 +1,11 @@
 
 package cs2731;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Utils
 {
@@ -30,6 +35,52 @@ public class Utils
 			}
 		}
 		return true;
+	}
+	
+	/**
+	 * Given a file, this method will return an array list of answers in that file.
+	 * Note:  This will break most likely if you give it a file that is not in the correct
+	 * format
+	 * @param answerFile A file in which the answers are extracted
+	 * @return The list of Answers
+	 * @throws FileNotFoundException
+	 */
+	static ArrayList<Answer> extractAnswers(File answerFile) throws FileNotFoundException {
+		ArrayList<Answer> answers = new ArrayList<Answer>();
+		Scanner fileInput = new Scanner(answerFile);
+		String filename = "";
+		int questionNumber;
+		int [] answerLines;
+		String questionText;
+		String [] answerTexts;
+		String firstLine; 
+		String [] explodedString;
+		
+		while(fileInput.hasNext()) {
+			firstLine = fileInput.nextLine();
+			if(firstLine.length() > 7) {
+				if(firstLine.substring(0, 6).equals("<FILE>")) 
+					filename = firstLine.substring(6);
+				else if(firstLine.substring(0, 10).equals("<Q_NUMBER>")) {
+					questionNumber = Integer.parseInt(firstLine.substring(10));
+					
+					explodedString = fileInput.nextLine().substring(8).split(",");
+					answerLines = new int [explodedString.length];
+					
+					for(int i = 0; i < explodedString.length; i++) 
+						answerLines[i] = Integer.parseInt(explodedString[i]);
+					
+					questionText = fileInput.nextLine().substring(7);
+					
+					explodedString = fileInput.nextLine().substring(7).split("  -OR- ");
+					answerTexts = explodedString;
+					
+					answers.add(new Answer(filename, questionNumber, answerLines, questionText, answerTexts));
+				}
+			}
+		}
+		
+		return answers;
 	}
 	
 }
